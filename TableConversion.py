444c22443ca74a42f5930f21325b0470414a53d9
vdveen <asvdveen@Annes-MacBook-Pro.local>
2014-10-07 14:22:10 -0600

    Added stuph

---
+++ TableConversion.py
@@ -0,0 +1,7 @@
+#-----------------------------------------------------
+#Name of script: Lab Assignment 3
+#Author: Andrew van der Veen, ID 10166883
+#Created: October 7th, 2014
+#Purpose: To convert coordinates in a csv file from DMS
+#         to the DD format and store it in a new file.
+#-----------------------------------------------------
