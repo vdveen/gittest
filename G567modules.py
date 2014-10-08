3647d8415261a9148d92735a94e6e4a89a2eee15
vdveen <asvdveen@Annes-MacBook-Pro.local>
2014-10-07 23:27:59 -0600

    Parsing argument succesfully
    but only if the coordinates are completely in the DMS format. If the
    seconds or minutes are missing, it doesn’t work (YET….)

--- G567modules.py
+++ G567modules.py
@@ -1,6 +1,7 @@
-def DMStoDD(d,m,s = 0):
+def DMStoDD(d,m = 0,s = 0):
   d = float(d)
-  m = float(m) / 60
+  if s <> 0:
+    m = float(m) / 60
   if s <> 0:
     s = float(s) / 60
   dms = (d + m + s)
--- TableConversion.py
+++ TableConversion.py
@@ -19,10 +19,33 @@ dmsfile = dmsfile.read()
 #Splits info from file into lines and puts into a list
 dmslines = dmsfile.split('\r')
 
-#Removes the first line
+#Removes the first line through slicing
 dmslines = dmslines[1:]
 
 #Go through file and split individual values
 for line in dmslines:
   values = line.split(',')
-  print values
+
+  #Chop the line in two to separate lat and lon values
+  length = len(values)
+  halfway = length / 2
+  lat = values[:halfway]
+  lon = values[halfway:]
+
+  #Parse latitude into function DMStoDD based on its length
+  if length == 6:
+    latDD = DMStoDD(lat[0], lat[1], lat[2])
+    lonDD = DMStoDD(lon[0], lon[1], lon[2])
+  elif length == 4:
+    latDD = DMStoDD(lat[0], lat[1])
+    lonDD = DMStoDD(lon[0], lon[1])
+  elif length == 2:
+    latDD = DMStoDD(lat[0])
+    lonDD = DMStoDD(lon[0])
+  else:
+    print 'Sorry, your latitude and longitude need to \
+    be of the same precision level! Input is either \
+    both DMS, DMS or both DM, DM or both D,D'
+
+  print latDD, lonDD
+#print latDD
