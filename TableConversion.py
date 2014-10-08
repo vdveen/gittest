3848da343dfa3d8abf4b7b110ae7f39df7938acc
vdveen <asvdveen@Annes-MacBook-Pro.local>
2014-10-07 23:58:04 -0600

    Got the values right
    Next up, I only have to get it in the output file. (“Only”…)

--- TableConversion.py
+++ TableConversion.py
@@ -11,7 +11,7 @@ from G567modules import DMStoDD
 
 #Opens csv file.
 dmsfile = open('dmscoords2.csv')
-#ddfile = open('ddcoords2.csv')
+#ddfile = open('ddcoords2.csv', 'w')
 
 #Reads information from file
 dmsfile = dmsfile.read()
@@ -36,28 +36,30 @@ for line in dmslines:
   lon = values[halfway:]
 
   #Parse coordinates into function DMStoDD.
-  #Basing it on length makes it possible to have
-  #no seconds, or to have only degrees as input.
   if length == 6:
+    #This converts line input to DD if degrees,
+    #minutes and seconds are given.
     latDD = DMStoDD(lat[0], lat[1], lat[2])
     lonDD = DMStoDD(lon[0], lon[1], lon[2])
     print latDD, lonDD
 
   elif length == 4:
+    #This converts line input to DD if degrees & minutes are given
     latDD = DMStoDD(lat[0], lat[1])
     lonDD = DMStoDD(lon[0], lon[1])
     print latDD, lonDD
 
   elif length == 2:
+    #This converts line input to DD if only degrees are given
     latDD = float(lat[0])
     lonDD = float(lon[0])
     print latDD, lonDD
 
   else:
+    #The only downside to chopping lists in half is that
+    #you can't have lat as DMS and lon as DM, for example.
+    #Hence why the above if statement uses multiples of 2.
     print 'Your coordinate is only', length,'value long.'
     print 'Sorry, the lat. and lon. need to have\n' \
     'the same amount of values for this to \n' \
     'work.'
-
-
-#print latDD
