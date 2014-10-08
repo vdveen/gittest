ff26443da130db4acfb6dd7124fd9ef941d83f4c
vdveen <asvdveen@Annes-MacBook-Pro.local>
2014-10-07 21:33:45 -0600

    Realized lists are iterable
    And file paths can be just local! Whoo.

--- TableConversion.py
+++ TableConversion.py
@@ -6,19 +6,12 @@
 #         to the DD format and store it in a new file.
 #-----------------------------------------------------
 
-#Opens csv file.
-cfile = open("/Users/asvdveen/Documents/Github/gittest/dmscoords2.csv", 'r')
-
-#Puts all lines in a single list
-coords = cfile.readlines()
-
-#Separates coords per line
-lines = str(coords).split('\\r')
-
 #Imports converter function from module.
 from G567modules import DMStoDD
 
-#Go through list and convert
-for line in lines:
+#Opens csv file.
+cfile = open("dmscoords2.csv")
+
+#Go through file and split longitude from latitude
+for line in cfile:
   print line
-#humm
